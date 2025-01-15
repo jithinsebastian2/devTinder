13b35127472f78f6cb4e6650f8e18c3cec5b8c32
jithinsebastian2 <jithinsebastian93@gmail.com>
2025-01-15 20:41:37 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -3,25 +3,13 @@ const express = require('express');
 const app = express();
 
 //This will only handle GET requests to /user route on this server
-app.get('/user', (req, res) => {
+app.get('/user/:userId/:name/:password', (req, res) => {
+    console.log("Query params", req.query);
+    console.log('Path params', req.params);
     res.send({
         firstName: 'Jithin',
-        lastName: 'Sebastian',
-        email: 'jithinsebastian93@gmail.com',
+        lastName: 'Sebastian'
     })
-});
-
-app.post('/user', (req, res) => {
-    res.send('Data saved successfully the the DB');
-});
-
-app.delete('/user', (req, res) => {
-    res.send('Data deleted successfully the the DB');
-});
-
-// This will match all the HTTP method API calls to /test
-app.use('/test', (req, res)=>{
-    res.send('Hello from test');
 });
 
 app.listen(3000, () => {
