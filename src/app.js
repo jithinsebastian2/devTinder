eaeed4013685e84b676ed1bbce8ee9f5f5ef0d89
jithinsebastian2 <jithinsebastian93@gmail.com>
2025-02-01 17:23:37 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -3,20 +3,68 @@ const connectDB = require('./config/database');
 const app = express();
 const User = require('./models/user');
 
+app.use(express.json());
+
 app.post('/signup', async (req, res)=> {
     //Creating a new instance of the user model
-    const user = new User({
-        firstName: 'Jithin',
-        lastName: 'Sebastian',
-        emailId: 'jithin@gmail.com',
-        password: 'jithin@1234'
-    });
+    const user = new User(req.body || {});
 
     try {
         await user.save();
         res.send('User added successfully');
     } catch(err) {
         res.status(400).send('Error saving the user: ' + err.message);
+    }
+});
+
+//Get User by email
+app.get('/user', async (req, res)=> {
+    try {
+        const users = await User.find({ emailId: req.body.emailId});
+        if (users.length > 0){
+            res.send(users);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch(err) {
+        res.status(400).send('Something went wrong');
+    }
+});
+
+// Find all the Users
+app.get('/feed', async (req, res)=> {
+    const users = await User.find({});
+    try {
+        res.send(users);
+    } catch (err){
+        res.status(400).send('Something went wrong');
+    }
+});
+
+//Delete User by default mongoDB _id
+app.delete('/user', async (req, res)=> {
+    console.log(req.body);
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete({ _id: userId });
+        res.status(200).send('User deleted successfully');
+    } catch (err){
+        res.status(400).send('Something went wrong');
+    }
+});
+
+//Update User data
+app.patch('/user', async (req, res)=> {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+            returnDocument: 'after',
+            runValidators: true, // It will validate the conditions for each fields in the schema while updating
+        });
+        res.status(200).send('User updated successfully');
+    } catch (err){
+        res.status(400).send('Update Failed'+ err.message);
     }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -4,22 +4,44 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
     firstName: {
         type: String,
+        required: true,
+        minLength: 4,
     },
     lastName: {
         type: String,
     },
     emailId: {
         type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
     },
     password: {
         type: String,
+        required: true,
     },
     age: {
         type: Number,
+        min: 18
     },
     gender: {
         type: String,
+        validate(value) {
+            if (!['male', 'female', 'others'].includes(value)) {
+                throw new Error('Gender data is not valid');
+            }
+        }
     },
+    about: {
+        type: String,
+        default: 'This is a default about for the user'
+    },
+    skills: {
+        type: [String],
+    }
+}, {
+    timestamps: true,
 });
 
 module.exports = mongoose.model('User', userSchema);
